128b45963be485679e5cd3a591ab78f99ffe1f40
Lody Borgers <hi@lodybo.nl>
2021-04-16 18:23:11 +0200

    LB-32 useGhostAuthor hook now uses the GraphQL query for the primary Ghost author.

--- src/components/common/hooks/ghostAuthor.ts
+++ src/components/common/hooks/ghostAuthor.ts
@@ -2,19 +2,15 @@ import { Author } from '@tryghost/content-api';
 import { graphql, useStaticQuery } from 'gatsby';
 
 export const useGhostAuthor = (): Author => {
-  const { authors } = useStaticQuery(
+  const { author } = useStaticQuery(
     graphql`
       query {
-        authors: allGhostAuthor(filter: { name: { eq: "Lody Borgers" } }) {
-          edges {
-            node {
-              ...GhostAuthorFields
-            }
-          }
+        author: ghostAuthor {
+          ...GhostAuthorFields
         }
       }
     `
   );
 
-  return authors.edges[0].node;
+  return author;
 };
